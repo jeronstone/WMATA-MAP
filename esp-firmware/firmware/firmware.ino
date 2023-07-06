8368e89c2bd831c615d4e6364b766f556c46fe0d
Josh Stone <jstone14@terpmail.umd.edu>
2023-07-05 22:15:09 -0400

    wowee

--- esp-firmware/firmware/firmware.ino
+++ esp-firmware/firmware/firmware.ino
@@ -14,8 +14,32 @@ void handleReceivedMessage(String message){
         return;
     }
 
-    const char * rd = parsed["RD"];  
-    Serial.println(rd);
+    const char * red_line = parsed["RD"];
+    
+    process(red_line);
+    
+    Serial.println(red_line);
+}
+
+void process(const char * str_in) {
+  if (strcmp(str_in, strstr(str_in, "!!!"))) {
+    Serial.println("Error: formatting");
+    return;
+  }
+
+  char * c = (char *) (str_in + 3);
+  float curr_pos[20]; // this is per line for now
+  int i = 0;
+  while (!strcmp(c, "!!!")) {
+    char c_cpy [10];
+    int j = 0;
+    while (c[0] != ';') {
+      c_cpy[j++] = c[0];
+      c++;
+    }
+    curr_pos[i++] = atof(c_cpy);
+    c++;
+  }
 }
 
 void setup() {
