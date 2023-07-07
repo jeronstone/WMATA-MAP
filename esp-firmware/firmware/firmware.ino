76a350d4f7930b305210d1d1d58d0e550a3f3352
Josh Stone <jstone14@terpmail.umd.edu>
2023-07-06 22:31:58 -0400

    finalize json sending, init leds

--- esp-firmware/firmware/firmware.ino
+++ esp-firmware/firmware/firmware.ino
@@ -1,13 +1,22 @@
+#include <FastLED.h>
 #include <WiFi.h>
 #include <WebSocketServer.h>
 #include <ArduinoJson.h>
 #include "secret_constants.h"
 
+
 WiFiServer server(80);
 WebSocketServer webSocketServer;
 
+void process(const char * str_in);
+
+double curr_pos[20];
+StaticJsonBuffer<2500> JSONBuffer;
+
+#define NUM_LEDS 1
+CRGB leds[NUM_LEDS];
+
 void handleReceivedMessage(String message){
-    StaticJsonBuffer<2500> JSONBuffer;
     JsonObject& parsed = JSONBuffer.parseObject(message);
     if (!parsed.success()) { 
         Serial.println("Parsing failed");
@@ -17,35 +26,45 @@ void handleReceivedMessage(String message){
     const char * red_line = parsed["RD"];
     
     process(red_line);
-    
-    Serial.println(red_line);
+
+    JSONBuffer.clear();
 }
 
-void process(const char * str_in) {
+void process(const char * str_in) { 
   if (strcmp(str_in, strstr(str_in, "!!!"))) {
-    Serial.println("Error: formatting");
+    Serial.println("Error: json format");
     return;
   }
 
   char * c = (char *) (str_in + 3);
-  float curr_pos[20]; // this is per line for now
   int i = 0;
-  while (!strcmp(c, "!!!")) {
+  while (strcmp(c, "!!!")) {
     char c_cpy [10];
     int j = 0;
     while (c[0] != ';') {
       c_cpy[j++] = c[0];
       c++;
     }
+    c_cpy[j] = '\0';
     curr_pos[i++] = atof(c_cpy);
     c++;
   }
+
+  // blink 1 for now
+  leds[0] = CRGB::Red;
+  FastLED.show();
+  delay(2000);
+  leds[0] = CRGB::Black;
+  FastLED.show();
 }
 
 void setup() {
- 
+
     Serial.begin(115200);
     delay(2000);
+
+    FastLED.addLeds<NEOPIXEL, 6>(leds, NUM_LEDS);
+    
     WiFi.begin(ssid, password); 
  
     while (WiFi.status() != WL_CONNECTED) {
@@ -73,7 +92,7 @@ void loop() {
     
             if (data.length() > 0) {
                 handleReceivedMessage(data);
-                webSocketServer.sendData(data);
+                webSocketServer.sendData("rx confirm");
             }
     
             delay(10);
